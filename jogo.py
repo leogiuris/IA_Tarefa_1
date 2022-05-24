0a6061464bb688a4344b853780dcc2019f53da0a
mcgema <mclarawerneck@gmail.com>
2022-05-24 10:36:33 -0300

    upgrade map interface

--- jogo.py
+++ jogo.py
@@ -4,25 +4,40 @@ BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
 WINDOW_HEIGHT = 400
 WINDOW_WIDTH = 400
-mapChar = []
+
 def GetMap():
+    mapChar = []
     f = open('MAPA_LENDA-AANG.txt','r')
-    lines = f.readlines()
+    lines = f.read().splitlines()
     for line in lines:
         lista = []
         for char in line:
             lista.append(char)
         mapChar.append(lista)
+    return mapChar
 
-      
-
+def pontos_get_map(map):
+    lista = []
+    soma = 0
+    for y in range(len(map)):
+        for x in range(len(map[y])):
+            count  = 0
+            for i in ['M', 'F', '.', 'A', 'R']:
+                if (map[y][x] == i):
+                    count = 1
+            if count == 0:
+                lista.append(map[y][x])
+            if map[y][x] == '\n':
+                soma +=1
+    print('soma ', soma)
+    return lista
 
 def drawGrid():
     #blockSizex = 5 #Set the size of the grid block
     blockSize = 1
-    GetMap()
-    for y in range(0, 82, blockSize): 
-       for x in range(0, 301, blockSize):
+    mapChar = GetMap()
+    for y in range(0, len(mapChar), blockSize): 
+       for x in range(0, len(mapChar[y]), blockSize):
             rect = pygame.Rect(x, y, blockSize, blockSize)
             if mapChar[y][x] == 'M':
                 pygame.Surface.fill(display, (255,248,220), rect, 1)
@@ -32,8 +47,11 @@ def drawGrid():
                 pygame.Surface.fill(display, (1,1,1), rect, 1)
             elif mapChar[y][x] == 'A':
                 pygame.Surface.fill(display, (0,0,1), rect, 1)
-            else:
+            elif mapChar[y][x] == 'R':
                 pygame.Surface.fill(display, (0,0,0), rect, 1)
+            else:
+                pygame.Surface.fill(display, (1,0,0), rect, 1)
+
 
 
 def main():
@@ -50,5 +68,3 @@ def main():
                 pygame.quit()
                 quit()
         pygame.display.update()
-
-main()
