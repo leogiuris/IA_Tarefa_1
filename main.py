3800a8bc3c191c1bab701a25e0c7ce8b22335e95
Pedro Chamberlain <pedrochmatos@gmail.com>
2022-06-02 10:14:42 -0300

    style(main): tidy up code

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
-
-from algo import *
-
-from jogo import *
-
+from algo import positions_etapas, astar, TIME
+from jogo import GetMap, RunView
 
 def main():
-
     totalPath = []
-
     map = GetMap()
     
     check_etapas = ['0', '1', '2', '3', '4', '5', '6', '7', '8',
-                    'B', 'C', 'D', 'E', 'G',
-                    'H', 'I', 'J', 'K', 'L',
-                    'N', 'O', 'P', 'Q', 'S', 
-                    'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    n = len(check_etapas)
-    
-    check_points = positions_etapas(map, check_etapas, n)
+                    'B', 'C', 'D', 'E', 'G', 'H', 'I', 'J', 'K', 
+                    'L', 'N', 'O', 'P', 'Q', 'S', 'T', 'U', 'V', 
+                    'W', 'X', 'Y', 'Z']
+    num_etapas = len(check_etapas)
+    check_points = positions_etapas(map, check_etapas, num_etapas)
 
-
-
-    for i in range(n-1):
+    for i in range(num_etapas - 1):
         path = astar(map, check_points[i], check_points[i+1])
         totalPath.append(path)
         print('caminho', i)
         print(path)
 
     print(TIME)
-
     print(totalPath)
     RunView(totalPath)
 
 if __name__ == '__main__':
     main()
-
-
