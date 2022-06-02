d673d7663b1b17e1f0dca629cd36cd56df96ad8b
Pedro Chamberlain <pedrochmatos@gmail.com>
2022-06-02 10:11:02 -0300

    refactor(jogo): organize file constants

--- jogo.py
+++ jogo.py
@@ -1,14 +1,18 @@
 import time
-
 import pygame
 
-BLACK = (0, 0, 0)
-WHITE = (200, 200, 200)
-GREEN = (0, 0, 255)
+BLACK = (  0,   0,   0)
+BLUE  = (  0,   0, 255)
+BROWN = (150,  75,   0)
+GREY  = (200, 200, 200)
+GREEN = (  0, 255,   0)
+PINK  = (255,   0, 220)
+RED   = (255,   0,   0)
+WHITE = (255, 255, 255)
+
 WINDOW_HEIGHT = 700
 WINDOW_WIDTH = 1200
-
-
+WINDOW_DIMENSIONS = (WINDOW_HEIGHT, WINDOW_WIDTH)
 
 def GetMap():
     mapChar = []
@@ -20,8 +24,6 @@ def GetMap():
             lista.append(char)
         mapChar.append(lista)
     return mapChar
-
-
 
 def pontos_get_map(map):
     lista = []
@@ -39,48 +41,40 @@ def pontos_get_map(map):
     print('soma ', soma)
     return lista
 
-
-
 def drawGrid():
-    #blockSizex = 5 #Set the size of the grid block
     blockSize = 4
     mapChar = GetMap()
     for y in range(0, len(mapChar)): 
        for x in range(0, len(mapChar[y])):
             rect = pygame.Rect(x*blockSize, y*blockSize*2, blockSize, blockSize*2)
             if mapChar[y][x] == 'M':
-                pygame.Surface.fill(display, (150,75,0), rect)
+                pygame.Surface.fill(display, BROWN, rect)
             elif mapChar[y][x] == 'F':
-                pygame.Surface.fill(display, (0,255,0), rect) 
+                pygame.Surface.fill(display, GREEN, rect) 
             elif mapChar[y][x] == '.':
-                pygame.Surface.fill(display, (255,255,255), rect)
+                pygame.Surface.fill(display, WHITE, rect)
             elif mapChar[y][x] == 'A':
-                pygame.Surface.fill(display, (0,0,255), rect)
+                pygame.Surface.fill(display, BLUE, rect)
             elif mapChar[y][x] == 'R':
-                pygame.Surface.fill(display, (0,0,0), rect)
+                pygame.Surface.fill(display, BLACK, rect)
             else:
-                pygame.Surface.fill(display, (255,0,0), rect)
-
-
+                pygame.Surface.fill(display, RED, rect)
 
 def drawPath(path):
     blockSize = 4
 
     for etapa in path:
-        
         for node in etapa:
             print(node)
             rect = pygame.Rect(node[0]*blockSize, node[1]*blockSize*2, blockSize, blockSize*2)
-            pygame.Surface.fill(display, (255, 0, 220), rect)
+            pygame.Surface.fill(display, PINK, rect)
             pygame.display.update()
     
-
-
 def RunView(path):
     global display
     drawn = False
     pygame.init()
-    display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    display = pygame.display.set_mode(WINDOW_DIMENSIONS)
     display.fill(BLACK)
     open =True
     while open:
@@ -95,4 +89,3 @@ def RunView(path):
                 pygame.quit()
                 quit()
         pygame.display.update()
-
